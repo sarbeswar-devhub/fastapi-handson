7d355d82a6c392cd1c3f6e2ace696e1ea3307b7b
sarbeswar3 <sarbeswar3@gmail.com>
2026-05-10 17:52:54 +0530

    updated route

--- app/routers.py
+++ app/routers.py
@@ -3,7 +3,7 @@ from app.modules.auth.router import router as auth_router
 from app.modules.user.router import router as user_router
 
 # config version 1.0 api routes
-api_router_v1 = APIRouter(prefix="/api/v1")
+api_router_v1 = APIRouter(prefix="/v1")
 
 # includes to verison 1.0 routes
 api_router_v1.include_router(auth_router)
